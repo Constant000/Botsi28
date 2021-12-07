09685393acdcf1e3e713c16e751e9c3adad4e711
Constant Dassonville <37074929+Constant000@users.noreply.github.com>
2021-12-07 03:42:49 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = "OTEyMzY5MTY3NDgxNTgxNjA4.YZu8AA._Q-dj0DAaOrBs5y53_Bt7_xG57U"
+const path = require('path');
+const ytdl = require('ytdl-core');
+
+
+client.once('ready', () => {
+   console.log('Félicitations, votre bot Discord a été correctement initialisé !');
+});
+
+client.login(process.env.token);
+
+client.on("message", message => {
+    if (message.content === "!indice") {
+      message.channel.send("rejoint le salon vocal est envoie moi !play, je te rejoindrai.")
+    }
+  })
+
+client.on("message", message => {
+  if (message.content === "!play") {
+    if (!message.member.voice.channel) return message.reply("You have to be in a VoiceChannel");
+      message.member.voice.channel.join().then(VoiceConnection => {
+          VoiceConnection.play("audio.mp3").on("finish", () => 
+          VoiceConnection.disconnect());
+          message.reply("done");
+    }).catch(e => console.log(e))
+  }
+})
