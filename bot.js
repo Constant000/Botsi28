c63ddd51a05c2aec6e5ffafa0fdc24a32a65a96f
Constant Dassonville <37074929+Constant000@users.noreply.github.com>
2021-12-21 10:11:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,17 +12,33 @@ client.login(process.env.token);
 
 client.on("message", message => {
     if (message.content === "!indice") {
-      message.channel.send("+play https://www.youtube.com/watch?v=PAiY4r3fI0k&ab_channel=ConstantDassonville")
+      message.channel.send("essaye de réussir le pacman, tu auras un indice à la fin, pour trouver le pacman")
+    }
+  })
+client.on("message", message => {
+    if (message.content === "!lien") {
+      message.channel.send("copie colle ce message : +play https://www.youtube.com/watch?v=PAiY4r3fI0k&ab_channel=ConstantDassonville")
+    }
+  })
+client.on("message", message => {
+    if (message.content === "!bonjour") {
+      message.channel.send("coucou toi, je suis Anne !, que fais tu sur ce discord ? c'est Kevin qui t'envoie ? (reponds par oui ou par non)")
+    }
+  })
+client.on("message", message => {
+    if (message.content === "oui") {
+      message.channel.send("Oh sympa ! il va bien ? tu veux des infos sur son projet de faire exploser BF ? (reponds par infos ou par non)")
+    }
+  })
+client.on("message", message => {
+    if (message.content === "non") {
+      message.channel.send("ok bah super t'as dead ca adios")
+    }
+  })
+client.on("message", message => {
+    if (message.content === "infos") {
+      message.channel.send("je n'ai pas confiance, il faut que tu joues au jeu de la confiance, kevin a toujours eu confiance aux gens qui gagne à pacman, pour cela lance le jeu")
     }
   })
 
-client.on("message", message => {
-  if (message.content === "!play") {
-    if (!message.member.voice.channel) return message.reply("You have to be in a VoiceChannel");
-      message.member.voice.channel.join().then(VoiceConnection => {
-          VoiceConnection.play(ytdl("https://www.youtube.com/watch?v=hEwqCjYeaFs&ab_channel=TIKTOKSOUND")).on("finish", () => 
-          VoiceConnection.disconnect());
-          message.reply("done");
-    }).catch(e => console.log(e))
-  }
-})
+
